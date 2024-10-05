b517da15de0bbd63e3877d4079b89776112a5e26
dhivyasankaran17 <157330214+dhivyasankaran17@users.noreply.github.com>
2024-10-04 20:33:28 -0400

    registrationNavigation
    Navigate registration Link

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework16 extends BaseTest{
+    @Test
+    public void registrationNavigation(){
+        //      Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+        //Registration Link
+        String regUrl = "https://qa.koel.app/registrion";
+
+
+    }
+
+}
