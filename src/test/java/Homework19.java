d4386ed495ab002b4e9def3a1e8189fefd71371e
dhivyasankaran17 <157330214+dhivyasankaran17@users.noreply.github.com>
2024-10-14 21:06:13 -0400

    DeletePlaylist
    Create playlist if playlist doesn't exist and Deleted Playlist

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -9,25 +10,55 @@ import java.time.Duration;
 public class Homework19 extends BaseTest{
     @Test
     public void deletePlaylist() throws InterruptedException {
-        //soundbarElement = "class.side player-controls";
+        String expectedDeletedPlaylistMsg = "Deleted playlist \"Playlist1.\"";
         provideEmail("dhivya.sankaran@testpro.io");
         providePassword("v5eUH9H2");
         clickLoginBtn();
+        Thread.sleep(2000);
+        if(!doesPlaylistExist()) {
+            // If there is a playlist then delete it
+            createPlaylist();
+        }
 
         selectPlaylist();
-        clickDeletePlaylist();
+
+        Assert.assertEquals(getDeletedPlaylistMsg(), expectedDeletedPlaylistMsg);
+
+    }
+    public boolean doesPlaylistExist()
+    {
+        // See if there is a playlist
+        By playlistLocator = By.xpath("//*[@id=\"playlists\"]/ul/li[3]/a");
+        return !driver.findElements(playlistLocator).isEmpty();
+    }
+    public void createPlaylist() throws InterruptedException{
+        WebElement createPlaylist = driver.findElement(By.xpath("//section[@id ='playlists']//i[@data-testid='sidebar-create-playlist-btn']"));
+        createPlaylist.click();
+        Thread.sleep(2000);
+        WebElement createSimplePlaylist = driver.findElement(By.xpath("//li[@data-testid='playlist-context-menu-create-simple']"));
+        createSimplePlaylist.click();
+        Thread.sleep(2000);
+        WebElement playlistField = driver.findElement(By.xpath("//input[@name='name']"));
+        playlistField.clear();
+        playlistField.sendKeys("Playlist1" + Keys.ENTER);
+        Thread.sleep(2000);
 
     }
 
-    public void clickDeletePlaylist() {
-        WebElement deletePlaylistBtn = driver.findElement(By.xpath("//button[@class='del btn-delete-playlist']"));
-        deletePlaylistBtn.click();
-    }
+    public void selectPlaylist() throws InterruptedException {
 
-    public void selectPlaylist()  {
-
-        WebElement playList = driver.findElement(By.cssSelector("//section[@id='playlists'] //a[contains(text(), 'Playlist1')]"));
+        WebElement playList = driver.findElement(By.xpath("//section[@id='playlists']//a[contains(text(), 'Playlist1')]"));
         playList.click();
-
+        Thread.sleep(2000);
+        WebElement deletePlaylistBtn = driver.findElement(By.cssSelector("button.del"));
+        deletePlaylistBtn.click();
+        Thread.sleep(2000);
+        /*WebElement deleteConfirmationBtn = driver.findElement(By.cssSelector("button.ok"));
+        deleteConfirmationBtn.click();
+        Thread.sleep(2000);*/
+    }
+    public String getDeletedPlaylistMsg(){
+        WebElement deleteMsg = driver.findElement(By.cssSelector("div.success.show"));
+        return deleteMsg.getText();
     }
 }
